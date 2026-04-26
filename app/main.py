ffbbb4250b19c3916b365be61e86d846014f6069
Ashen344 <ashenc2004@gmail.com>
2026-04-26 13:32:11 +0530

    m

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# Import all route modules
+# Import all route module
 from app.routes.contracts import router as contracts_router
 from app.routes.auth import router as auth_router
 from app.routes.ai import router as ai_router
